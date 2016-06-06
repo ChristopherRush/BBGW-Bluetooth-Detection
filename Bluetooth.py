07906308bc1a74d03fd4b273949643fe20a6778a
ChristopherRush <chris.uclan@gmail.com>
2016-06-06 11:00:26 +0100

    Update

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,13 +1,16 @@
 import os
 import bluetooth, time
 
+#Time spent looking for local Bluetooth Devices
 search_time = 10
 
 # You can hardcode the desired device ID here as a string to skip the discovery stage
 addr = None
+
 print("Enable Bluetooth module with the follwing command: sudo bb-wl18xx-bluetooth")
 print("Make sure your desired Bluetooth-capable device is turned on and discoverable.")
 
+#If the address has not been hardcoded above then run the following script
 if addr == None:
     try:
         input("When you are ready to begin, press the Enter key to continue...")
@@ -15,6 +18,7 @@ if addr == None:
         pass
 
     os.system('clear')
+# A simple menu to select from a stored text file or scan new devices
     ans  = input("Select your option from the menu: \n1. Scan for new Bluetooth Devices\n2. Select Bluetooth device from menu\n")
     if ans == 1:
 
@@ -22,7 +26,7 @@ if addr == None:
         print("Searching for Bluetooth devices...")
 
         nearby_devices = bluetooth.discover_devices(duration=search_time, flush_cache=True, lookup_names=True)
-
+# If no devices have been found loop the scan to search again
         while len(nearby_devices) <= 0:
                 input("No Devices Found, Make sure you Bluetooth Device is turned on \nPress Enter to Scan again....")
                 os.system('clear')
@@ -32,21 +36,22 @@ if addr == None:
 
 
         os.system('clear')
+# If devices have been found run the follwing
         if len(nearby_devices) > 0:
             print("Found %d devices!" % len(nearby_devices))
 
 
-        i = 0 # Just an incrementer for labeling the list entries
+            i = 0
         # Print out a list of all the discovered Bluetooth Devices
-        for addr, name in nearby_devices:
-            print("%s. %s - %s" % (i, addr, name))
-            i =+ 1
+            for addr, name in nearby_devices:
+                print("%s. %s - %s" % (i, addr, name))
+                i =+ 1
 
-        device_num = input("Please specify the number of the device you want to track: ")
+            device_num = input("Please specify the number of the device you want to track: ")
 
-        # extract out the useful info on the desired device for use later
-        addr, name = nearby_devices[device_num][0], nearby_devices[device_num][1]
 
+            addr, name = nearby_devices[device_num][0], nearby_devices[device_num][1]
+# Add the selected Bluetooth device to the next line in the text file
         f = open( 'file.txt', 'a' )
         f.write( addr + ',' + name + '\n' )
         f.close()
@@ -54,27 +59,28 @@ if addr == None:
 
     if ans == 2:
         os.system('clear')
-
+# Read the text file for a list of saved Bluetooth Devices
         f = open( 'file.txt', 'r')
         line = f.readline()
 
         z = 0
-
+# Print the list using this loop, incrementing the value by line
         while line:
             z += 1
             print z , ". " , line
             line = f.readline()
         f.close()
 
+# Prompt the user to enter the device number from the list
         f = open( 'file.txt', 'r')
         opt = input("")
 
         x = 0
-
+# Read the line selected and extract the address of the device by splitting the line
         while x < opt:
-         readit = f.readline().split(",")[0]
-         x += 1
-        addr = readit
+            readit = f.readline().split(",")[0]
+            x += 1
+            addr = readit
         f.close()
 
 os.system('clear')
@@ -82,10 +88,9 @@ print("Scanning for device %s." % (addr))
 print("Initializing Scan...")
 
 
+
 while True:
-    # Try to gather information from the desired device.
-    # We're using two different metrics (readable name and data services)
-    # to reduce false negatives.
+
     state = bluetooth.lookup_name(addr, timeout=20)
     services = bluetooth.find_service(address=addr)
 
@@ -95,5 +100,6 @@ while True:
     else:
         print("Device detected!")
 
-    # Arbitrary wait time
+
     time.sleep(1)
+    
